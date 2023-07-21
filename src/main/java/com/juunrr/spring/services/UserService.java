2bbfa57b82cd507ccbf7eb2e5843b82ed4ffbd1d
Juan Ribeiro Rodrigues <juancontato27@gmail.com>
2023-07-20 23:53:20 -0300

    User delete

--- src/main/java/com/juunrr/spring/services/UserService.java
+++ src/main/java/com/juunrr/spring/services/UserService.java
@@ -3,8 +3,11 @@ package com.juunrr.spring.services;
 import com.juunrr.spring.entities.User;
 import com.juunrr.spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,4 +30,9 @@ public class UserService {
     public User insert(User obj){
         return repository.save(obj);
     }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
 }
